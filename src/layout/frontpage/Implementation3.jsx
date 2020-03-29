28227fefb11d14aacf3cc14e2ba805a250463f1e
Lukasz <lukzerom@gmail.com>
2020-03-29 14:48:23 +0200

    Implementation 3 ready

--- src/layout/frontpage/Implementation3.jsx
+++ src/layout/frontpage/Implementation3.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import '../../css/frontpage/implementation3.css'
+import img34 from '../../assets/thumbnails/34.jpg'
+import img35 from '../../assets/thumbnails/35.jpg'
+import img36 from '../../assets/thumbnails/36.jpg'
 
 const Implementation3 = () => {
-  return <div></div>;
+  return (
+    <>
+    <div className="implementation3">
+<div className="div1"> <h1>Esotiq</h1> </div>
+<div className="div2"> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi expedita labore aliquam quidem commodi officiis suscipit. Illum possimus recusandae corrupti officiis corporis ad laborum eligendi fugit, accusamus, esse beatae reprehenderit, minus obcaecati cupiditate at placeat dicta. Illum velit ratione, fugiat eligendi quibusdam, maiores quis, quidem saepe aliquam consequuntur sit distinctio Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quaerat inventore necessitatibus magni sint. Consectetur accusantium soluta odio possimus natus. </p></div>
+<div className="div3">  <h4 className="sideText">Esotiq</h4>  <img src={img34} alt="" className="img"/> </div>
+<div className="div4">  <h4 className="sideText">Esotiq</h4>  <img src={img35} alt="" className="img"/>  </div>
+
+</div>
+<img className="img" src={img36} alt=""/>
+</>
+  )
 };
 
 export default Implementation3;
