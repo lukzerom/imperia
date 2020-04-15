8676b4c7b212baf78a35243677d9091ac6ca0b7f
Lukasz <lukzerom@gmail.com>
2020-04-15 21:48:13 +0200

    Added map to contact

--- src/layout/contact/Contact.jsx
+++ src/layout/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Map from "./Map";
 
 const Contact = () => {
   return (
     <div>
       <div className="divider"></div>
       <h1>Kontakt</h1>
+      <Map />
     </div>
   );
 };
---
+++ src/layout/contact/Map.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import "../../css/contact.css";
+import L from "leaflet";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
+
+class Map extends React.Component {
+  componentDidMount() {
+    this.map = L.map("map", {
+      center: [54.41166, 18.568942],
+      zoom: 15,
+      zoomControl: false,
+    });
+
+    L.tileLayer(
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+      {
+        detectRetina: true,
+        maxZoom: 20,
+        maxNativeZoom: 17,
+      }
+    ).addTo(this.map);
+    L.marker([54.41166, 18.568942]).addTo(this.map);
+  }
+
+  render() {
+    return <Wrapper width="100%" height="500px" id="map" />;
+  }
+}
+
+export default Map;
